e2ffc5f0db6333867026494ff84ec8cd39784bc7
Raúl González <gonzalv@ciencias.unam.mx>
2021-01-16 14:23:36 -0600

    Agrega archivos base para lexer y parser

---
+++ front-end/lexer.rkt
@@ -0,0 +1,15 @@
+#lang nanopass
+
+#|
+Compiladores 2021-1
+
+Autores:
+313245312 - González Alvarado Raúl
+316255709 - Hernández Cano Alejandro
+xxxxxxxxx - Miguel
+xxxxxxxxx - Miren
+
+Lexer para archivos del lenguaje LF (extensión .mt)
+|#
+
+(provide (all-defined-out))
---
+++ front-end/parser.rkt
@@ -0,0 +1,18 @@
+#lang nanopass
+
+#|
+Compiladores 2021-1
+
+Autores:
+313245312 - González Alvarado Raúl
+316255709 - Hernández Cano Alejandro
+xxxxxxxxx - Miguel
+xxxxxxxxx - Miren
+
+Parser para archivos del lenguaje LF (extensión .mt)
+|#
+
+(provide (all-defined-out))
+
+; TODO: El parser debería regresar una lista de símbolos
+; que pueda ser procesada por parser-LF
